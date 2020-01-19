c6c8d260edfcb21e597471fb62307eebfabc53d7
Joe <rice.joseph@titans.easternflorida.edu>
2020-01-19 13:59:54 -0500

    Adding new objects in .java file

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.tipcalculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText billAmountEditText;
+    private TextView tipTextView;
+    private TextView totalTextView;
+
+    private TextView percentTextView;
+
+    private Button percentUpButton;
+    private Button percentDownButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        billAmountEditText = (EditText)findViewById(R.id.billAmountEditText);
+        percentTextView = (TextView)findViewById(R.id.percentTextView);
+        tipTextView = (TextView)findViewById(R.id.tipTextView);
+        totalTextView = (TextView)findViewById(R.id.totalTextView);
+
+        percentUpButton = (Button) findViewById(R.id.percentUpButton);
+        percentDownButton = (Button) findViewById(R.id.percentDownButton);
+
     }
 }
